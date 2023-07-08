987bdc75a81a2f1ecd97b87a77414a4d22f58994
Sobiyk <kirill.sobkovskii@yandex.ru>
2023-07-08 05:42:54 +0300

    frontend added

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -64,12 +64,18 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foodgram',
-        'USER': 'postgres',
-        'PASSWORD': 'KiRyHa99',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': os.getenv('DB_ENGINE',
+                            default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME',
+                          default='database'),
+        'USER': os.getenv('POSTGRES_USER',
+                          default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD',
+                              default='postgres'),
+        'HOST': os.getenv('DB_HOST',
+                          default='db'),
+        'PORT': os.getenv('DB_PORT',
+                          default='5432'),
     }
 }
 
